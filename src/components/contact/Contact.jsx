eebfa9e0ff67ddbd8a15b4ac1bc3ec2617db6deb
TosinMary <teetoastl3@gmail.com>
2024-05-19 15:28:02 +0100

    css styling modification

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,26 @@
+import { useRef } from "react"
 import React from 'react'
 import "./Contact.css"
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+        publicKey: 'YOUR_PUBLIC_KEY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <>
     <div className='contact-container' id='contact'>
@@ -9,7 +28,7 @@ function Contact() {
         <h3 className='contact-header'>Contact Me</h3>
         <p className='contact-paragraph'>Please fill out the form below to discuss any job opportunities</p>
         <section className='form-layout'>
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
                <input type='text' placeholder="Your name"/>
                 <input type='email'  placeholder="Your email"/>
                <textarea name='message' row="5" placeholder="Your message"></textarea>
