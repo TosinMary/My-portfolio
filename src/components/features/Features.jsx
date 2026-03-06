6bee0cd333002c6377a83a2c72f497ff3877cec5
TosinMary <teetoastl3@gmail.com>
2026-03-06 09:56:12 +0000

    project sample update

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -14,7 +14,7 @@ const Features = () => {
        <section className='project-container'>
             <div className='project'>
                 <div className='img-container'>
-                  <h4>Zedu App Test Cases</h4>
+                  <h4>Test Cases</h4>
                 </div>
                 <div className='project-summary'>
                   <div >
@@ -24,7 +24,7 @@ const Features = () => {
                 </div>
             </div> <div className='project'>
                 <div className='img-container'>
-                  <h4>Gradiffic App Test Cases</h4>
+                  <h4>Test Cases</h4>
                 </div>
                 <div className='project-summary'>
                   <div >
@@ -35,39 +35,39 @@ const Features = () => {
             </div>
              <div className='project'>
                 <div className='img-container'>
-                  <h4>API Automation Link with Postman</h4>
+                  <h4>API Automation with Postman</h4>
                 </div>
                 <div className='project-summary'>
                   <div >
-                   <h4>Zedu App API Automation Link: <a href='https://www.postman.com/api-test-1186/workspace/zedu/collection/42497516-cce2356d-6dbb-40c3-88ee-b790282f7c29?action=share&source=copy-link&creator=42497516' className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4>
+                   <h4>API Automation: <a href='https://www.postman.com/api-test-1186/workspace/zedu/collection/42497516-cce2356d-6dbb-40c3-88ee-b790282f7c29?action=share&source=copy-link&creator=42497516' className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4>
                   </div>
                 </div>
             </div>
              <div className='project'>
                 <div className='img-container'>
-                  <h4>Sample Test Plan</h4>
+                  <h4>Test Plan</h4>
                 </div>
                 <div className='project-summary'>
                   <div >
-                   <h4>Telex App Test Plan Doc: <a href='https://docs.google.com/document/d/12a5yGWC5Uct5lvKhu4-JhK2fbalOsxaRNj74mAu281w/edit?usp=sharing' className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4> <br></br>
-                    <h4>Delve App Test Plan Doc: <a href='https://docs.google.com/document/d/1HEbXRRAHDmnChEyl1kE0cUXgSYN2OPwx8V92mmV1hlo/edit?usp=sharing'className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4>
+                   <h4>Test Plan Doc: <a href='https://docs.google.com/document/d/12a5yGWC5Uct5lvKhu4-JhK2fbalOsxaRNj74mAu281w/edit?usp=sharing' className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4> <br></br>
+                    <h4>Test Plan Doc: <a href='https://docs.google.com/document/d/1HEbXRRAHDmnChEyl1kE0cUXgSYN2OPwx8V92mmV1hlo/edit?usp=sharing'className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4>
                   </div>
                 </div>
             </div>
             <div className='project'>
                 <div className='img-container'>
-                  <h4>Sample Bug Report</h4>
+                  <h4>Bug Report</h4>
                 </div>
                 <div className='project-summary'>
                   <div >
-                   <h4>TCN App Bug Report: <a href='https://docs.google.com/spreadsheets/d/1TlcBE5JuGI3nrOYt04eqRx8XVK2Z46zRlKz_gYtIvlc/edit?usp=sharing' className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4> <br></br>
-                    <h4>Telex API Bug Report: <a href='https://docs.google.com/spreadsheets/d/1jZL_R9qFHs0dz-j-C-2__Dp0of4dN1gchQ3F63jiMUk/edit?usp=sharing'className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4>
+                   <h4>Bug Report: <a href='https://docs.google.com/spreadsheets/d/1TlcBE5JuGI3nrOYt04eqRx8XVK2Z46zRlKz_gYtIvlc/edit?usp=sharing' className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4> <br></br>
+                    <h4>API Bug Report: <a href='https://docs.google.com/spreadsheets/d/1jZL_R9qFHs0dz-j-C-2__Dp0of4dN1gchQ3F63jiMUk/edit?usp=sharing'className='project-icon' target='_blank'><FaExternalLinkAlt /></a></h4>
                   </div>
                   </div>
                   </div>
                     <div className='project'>
                 <div className='img-container'>
-                  <h4>PetStore App Automation with RestAsured, Java and TestNG</h4>
+                  <h4>BDD Automation with RestAsured, Java and TestNG</h4>
                 </div>
                 <div className='project-summary'>
                   <div >
